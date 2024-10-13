02c615c16192c4f97ce40c21269fd54803ac172c
Atlas <atlasyigitaydin@gmail.com>
2024-10-13 17:49:11 +0300

    ı want to play a game

--- components/CustomInput.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-const emit = defineEmits(["update:modelValue"]);
-</script>
-
-<template>
-  <input :value="modelValue" @input="emit('update:modelValue')" />
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,19 @@
 <script setup lang="ts">
+definePageMeta({
+  name: 'home',
+  path: '/',
+})
 
-const message = ref("Nuxt");
-
-function hello() {
-  sayHello(message.value);
-}
-
-const { data } = await useFetch('/api/hello')
-
+// GÖREV 'istediğin zaman yapabilirsin'
+// bu sayfa için görevin kendini burda başarılı bir şekilde tanıtman
+// değerlendireceğim şey tasarım ve doğru component kullanımı olacak
 </script>
 
 <template>
-  <NuxtLink to="/about">GoTo About Page / </NuxtLink>
-  <NuxtLink to="/todo">   GoTo TODO Page / </NuxtLink>
-  <NuxtLink to="/quote">   GoTo Quote Page</NuxtLink>
-
-
-    <UAlert
-      title="Customize Alert Avatar"
-      description="Insert custom content into the avatar slot!"
-      :avatar="{
-        src: 'https://avatars.githubusercontent.com/u/739984?v=4',
-        alt: 'Avatar',
-      }"
-    >
-      <template #avatar="{ avatar }">
-        <UAvatar
-          v-bind="avatar"
-          chip-color="primary"
-          chip-text=""
-          chip-position="top-right"
-        />
-      </template>
-    </UAlert>
-  <div>
-    <UButton color="primary" variant="solid">Button</UButton>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert> This is an auto-imported component </AppAlert>
-  </div>
-
-  <h1>Demo with auto imports</h1>
-  <form class="flex gap-2" @submit.prevent="hello">
-    <CustomInput v-model="message" />
-    <UButton type="submit"> Hello </UButton>
-  </form>
-
-
-  <div>
-    <p>
-      Result of <UKbd
-        size="md"
-        value="/api/hello"
-      />:
-    </p>
-    <pre>{{ data }}</pre>
-  </div>
-
-
+  <NuxtLayout>
+    <div class="w-full h-full flex justify-center items-center gap-6">
+      <ButtonGiant label="Todo List" @click="navigateTo({ name: 'todo' })" />
+      <ButtonGiant label="Pokemon List" @click="navigateTo({ name: 'pokemon' })" />
+    </div>
+  </NuxtLayout>
 </template>
-
-<style>
-body {
-  background-color: rgb(124, 163, 163);
-}
-</style>
