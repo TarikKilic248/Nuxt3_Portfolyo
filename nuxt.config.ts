2b41cd26ba88801d43e805813d24c0622d48e6b0
Tarık Kılıç Alsancak <batmaca2002@gmail.com>
2024-11-20 14:12:50 +0300

    static HTML export with Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,10 +18,15 @@ export default defineNuxtConfig({
   },
 
   app: {
-
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      external: ['pinia'],
     },
   },
 })
